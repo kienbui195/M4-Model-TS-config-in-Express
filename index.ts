72146f5a33b766da77435ce255dfe7544028f75d
Kay Bi <kaybi9x@gmail.com>
2022-09-08 22:58:19 +0700

    update

--- index.ts
+++ index.ts
@@ -1,15 +1,42 @@
-import express from 'express';
-import bodyParser from "body-parser";
-const port = 8000;
-const app = express();
+import * as express from "express";
+import {Request, Response} from "express";
+import {AppDataSource} from "./src/data-source";
+import {User} from "./src/entity/User";
 
-app.use(bodyParser.json());
-app.get('/', (req, res) => {
-    res.end('<h1>Hello World</h1>')
-})
+AppDataSource.initializa().then(async connection => {
+    const userRepository = connection.getRepository(User);
+    const app = express();
+    app.use(express.json());
 
+    app.get("/users", async function (req: Request, res: Response) {
+        const users = await userRepository.find();
+        res.json(users);
 
+        app.get("/users/:id", async function (req: Request, res: Response) {
+            const results = await userRepository.findOne(req.params.id);
+            return res.send(results);
+        });
 
-app.listen(port, 'localhost', () => {
-    console.log(`running at http://localhost:${port}`);
+        app.post("/users", async function (req: Request, res: Response) {
+            const user = await userRepository.create(req.body);
+            const results = await userRepository.save(user);
+            return res.send(results);
+        });
+
+        app.put("/users/:id", async function (req: Request, res: Response) {
+            const user = await userRepository.findOne(req.params.id);
+            userRepository.merge(user, req.body);
+            const results = await userRepository.save(user);
+            return res.send(results);
+        });
+
+        app.delete("/users/:id", async function (req: Request, res: Response) {
+            const results = await userRepository.delete(req.params.id);
+            return res.send(results);
+        });
+
+        app.listen(8080, 'localhost', () => {
+            console.log(`running at http://localhost:8080`);
+        })
+    })
 })
